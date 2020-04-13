e5aae392fb51501c1838e3e515545c2a850df084
Hong0330 <gjku5561@naver.com>
2020-04-13 13:41:43 +0900

    add add() function

--- hello.c
+++ hello.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
+int add(int a, int b) {
+	return a + b;
+}
+
 void swap() {
 	return 0;
 }
 
 int main() {
 	printf("Hello World!!!!\n");
+	
+	int a = 1;
+	int b = 2;
+
+	printf("a + b = %d \n", add(a,b));
 
 	return 0;
 }
